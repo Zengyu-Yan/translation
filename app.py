1b298ebed000e0de67120316bfc07da6d516568b
Zengyu-Yan <73959962+Zengyu-Yan@users.noreply.github.com>
2022-12-16 23:33:26 +0000

    last_edit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-#import pandas as pd
+import pandas as pd
 
 # Create the Flask app
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Load the dataframe with the translation pairs
 df = pd.read_csv("cleaned_dataset.csv")
 
-@app.route('/translations')
+@app.route("/")
 def get_translations():
     # Get the first 10 rows from the dataframe
     translations = df.head(10)
@@ -19,4 +19,4 @@ def get_translations():
     return jsonify(translations_dict)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, debug=True)
