d9f3d86caaa95b234eab504f53bd5ed5b96f81fa
Zengyu-Yan <73959962+Zengyu-Yan@users.noreply.github.com>
2022-12-19 09:44:33 +0100

    update file

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ app = Flask(__name__)
 # Load the dataframe with the translation pairs
 df = pd.read_csv("cleaned_dataset.csv")
 
-@app.route("/")
+@app.route("/api/v1/status", methods=['GET'])
+def status():
+    # Return appropriate status code
+    return jsonify({'status': 'OK'}), 200
+
+@app.route('/api/v1/sentences', methods=['GET'])
 def get_translations():
     # Get the first 10 rows from the dataframe
     translations = df.head(10)
@@ -16,7 +21,7 @@ def get_translations():
     translations_dict = translations.to_dict()
 
     # Return the dictionary as a JSON response
-    return jsonify(translations_dict)
+    return jsonify(translations_dict), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
